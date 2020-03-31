684b6f902b87038161e9c1373aadafb62c440210
Pavel Stránský <github@pavelstransky.cz>
2020-03-31 13:06:48 +0200

    Fixed error in operator n

--- models/Vibron.jl
+++ models/Vibron.jl
@@ -5,9 +5,10 @@ function Energy(x, parameters)
     P, p, Q, q = x
     A, B, C = parameters
 
-    s2 = 2.0 - (p*p + P*P + q*q + Q*Q)
+    Σ2 = p*p + P*P + q*q + Q*Q
+    s2 = 2.0 - Σ2
 
-    return A * s2 + B * ((P*P + p*p) * s2 + (P*q - p*Q) ^ 2) + C * P * sqrt(s2)
+    return A * Σ2 + B * ((P*P + p*p) * s2 + (P*q - p*Q) ^ 2) + C * P * sqrt(s2)
 end
 
 """ Filling initial conditions (at a given energy calculates randomly all missing points) """
@@ -31,7 +32,7 @@ function InitialCondition!(x0, e, parameters)
             return true
         end
 
-        discriminant = ((A + B * p*p) * s2 - e) / A
+        discriminant = (e - (A * (p*p + q*q) + B * p*p * s2)) / A
         if discriminant < 0
             return false
         end
@@ -77,24 +78,24 @@ function EquationOfMotion!(dx, x, parameters, t)
     Cs = C / s
     Cs3 = Cs / s2
 
-    dx[1] = A2 * Q + B2 * P * b + Cs * P * Q
-    dx[2] = A2 * q + B2 * p * b + Cs * P * q
-    dx[3] = -A2 * P - B2 * (2.0 * P * (a - 1.0) + Q * b) + Cs * (s2 - P*P)
-    dx[4] = -A2 * p - B2 * (2.0 * p * (a - 1.0) + q * b) - Cs * p * P
+    dx[1] = -A2 * Q + B2 * P * b + Cs * P * Q
+    dx[2] = -A2 * q + B2 * p * b + Cs * P * q
+    dx[3] = A2 * P - B2 * (2.0 * P * (a - 1.0) + Q * b) + Cs * (s2 - P*P)
+    dx[4] = A2 * p - B2 * (2.0 * p * (a - 1.0) + q * b) - Cs * p * P
 
     HPQ = -B2 * (p * q + 2.0 * P * Q) - Cs3 * Q * (s2 + P*P)
     HpQ = -B2 * P * q - Cs3 * p * P * Q
-    HQQ = -A2 - B2 * P*P - Cs3 * P * (s2 + Q*Q)
+    HQQ = A2 - B2 * P*P - Cs3 * P * (s2 + Q*Q)
     HQq = -B2 * p * P - Cs3 * P * q * Q
 
     HPq = -B2 * p * Q - Cs3 * q * (s2 + P*P)
     Hpq = -B2 * (2.0 * p * q + P * Q) - Cs3 * p * P * q
-    Hqq = -A2 - B2 * p*p - Cs3 * P * (s2 + q*q)
+    Hqq = A2 - B2 * p*p - Cs3 * P * (s2 + q*q)
 
-    HPP = -A2 - B2 * (6.0 * P*P + 2.0 * p*p + Q*Q - 2.0) - Cs3 * P * (P*P - 3.0 * a + 3.0 * (s2 + a))
+    HPP = A2 - B2 * (6.0 * P*P + 2.0 * p*p + Q*Q - 2.0) - Cs3 * P * (P*P - 3.0 * a + 3.0 * (s2 + a))
     HPp = -B2 * (4.0 * p * P + q * Q) - Cs3 * p * (s2 + P*P)
 
-    Hpp = -A2 - B2 * (2.0 * P*P + 6.0 * p*p + q*q - 2.0) - Cs3 * P * (s2 + p*p)
+    Hpp = A2 - B2 * (2.0 * P*P + 6.0 * p*p + q*q - 2.0) - Cs3 * P * (s2 + p*p)
     
     j = [-HPQ -HpQ -HQQ -HQq;
          -HPq -Hpq -HQq -Hqq;
