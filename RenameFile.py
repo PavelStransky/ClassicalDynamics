a33f4ad5c6b78949108871aaa81bcff6daac7a3f
Pavel Stránský <github@pavelstransky.cz>
2022-03-15 10:40:34 +0100

    Rename ouptut png files

---
+++ RenameFile.py
@@ -0,0 +1,23 @@
+import os
+
+fnames = []
+energies = []
+
+dir = 'd:/results/Vibron/freg/A=0.4'
+
+for fname in os.listdir(dir):
+    start = fname.find("]_E=") + 4
+    end = fname.find(".png")
+
+    fnames.append(fname)
+    energies.append(float(fname[start:end]))
+
+fnames = [x for _, x in sorted(zip(energies, fnames))]
+print(fnames)
+
+for i, fname in enumerate(fnames):
+    src = f'{dir}/{fname}'
+    dst = f'{dir}/{i}.png'
+
+    os.rename(src, dst)
+
