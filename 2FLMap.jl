19866c87007f4a4d9306c64d5b2224e21fe6dee3
Pavel Stránský <github@pavelstransky.cz>
2022-05-25 14:06:36 +0200

    Map for a given section

---
+++ 2FLMap.jl
@@ -0,0 +1,120 @@
+using Distributed
+using Random
+using Logging
+
+workers = 24
+
+if nprocs() <= workers
+    addprocs(workers + 1 - nprocs())
+end
+
+@everywhere include("models/TwoFluidLipkin.jl")
+@everywhere include("modules/ClassicalDynamics.jl")
+
+@everywhere disable_logging(Logging.Info)
+
+""" Calculates Poincaré sections with Lyapunov exponents for various energies
+    Parallel calculation, takes usually days to finish
+"""
+
+@everywhere function SolveItem(energy, parameters, dimension; file="2FL.txt", path="", alreadySolved=[])    
+    for (E, λ) in alreadySolved
+        if isapprox(E, energy) && isapprox(parameters[1], λ)
+            println("Skipped $parameters, E=$energy, dim=$dimension (already calculated)")
+            return false
+        end
+    end
+
+    time = @elapsed averageLyapunov, freg, trajectories, lyapunovs = SolveEnergy(energy, parameters, dimension, savePath=path, timeout=10000, showFigures=false, randomize=true, tolerance=5e-9, sectionCoordinateX=1, sectionCoordinateY=3, sectionPlane=2)
+
+    chaos = 0
+    total = 0
+    error = 0
+    meanLyapunov = 0
+    meanLyapunovChaos = 0
+
+    for x in averageLyapunov
+        if x > 0.0
+            total += 1
+            meanLyapunov += x
+        end
+        if x == 0.0
+            error += 1
+        end
+        if x > 0.01
+            chaos += 1
+            meanLyapunovChaos += x
+        end        
+    end    
+
+    varλ = 0
+    if length(lyapunovs) == 0
+        maxλ = 0
+        meanλ = 0
+    else
+        maxλ = maximum(lyapunovs)
+        meanλ = mean(lyapunovs)
+        if length(lyapunovs) > 1
+            varλ = var(lyapunovs)
+        end
+    end
+
+    result = [energy, parameters[1], total, chaos, error, freg, total > 0 ? meanLyapunov / total : 0, chaos > 0 ? meanLyapunovChaos / chaos : 0, length(lyapunovs), maxλ, meanλ, varλ, myid(), time, trajectories]
+
+    open(path * file, "a") do io
+        println(io, result)
+    end
+
+    return true
+end
+
+function RunMap(; η1=0.5, η2=-0.5, path="", dimension=101, step=0.1)
+    path *= "2FT_"
+
+    file = "Map_dim=$(dimension)_x1=$(η1)_x2=$(η2).txt"
+    alreadySolved = ReadMap(path * file)
+
+    input = shuffle([(energy, [ξ, η1, η2], dimension) for energy in -1.2:step:1, ξ in 0:step:1])
+
+    println("To be calculated $(length(input)).")
+    println("Already calculated $(length(alreadySolved)) points.")
+
+    pmap((args)->SolveItem(args...; file=file, path=path, alreadySolved=alreadySolved), input)
+
+    return
+end
+
+""" Calculates freg for one given set of parameters """
+function RunEnergy(; ξ=0.5, η1=0.5, η2=-0.5, path="", dimension=101, step=0.01)
+    path *= "2FL_"
+
+    file = "Energy_dim=$(dimension)_$([ξ, η1, η2]).txt"
+    alreadySolved = ReadMap(path * file)
+
+    input = [(energy, [ξ, η1, η2], dimension) for energy in -1:step:1]
+
+    println("To be calculated $(length(input)).")
+    println("Already calculated $(length(alreadySolved)) points.")
+
+    pmap((args)->SolveItem(args...; file=file, path=path, alreadySolved=alreadySolved), input)    
+
+    return
+end
+
+function ReadMap(file="")
+    Eλ = []
+
+    try
+        for line in eachline(file)     
+            line = replace(line, "[" => "")   
+            line = replace(line, "]" => "")
+            line = replace(line, "," => "")
+            elements = split(line)
+
+            append!(Eλ, [(parse(Float64, elements[1]), parse(Float64, elements[2]))])
+        end
+    catch x
+    end
+
+    return Eλ
+end
--- 2FLSection.jl
+++ 2FLSection.jl
@@ -3,8 +3,8 @@ using Plots
 include("models/TwoFluidLipkin.jl")
 include("modules/ClassicalDynamics.jl")
 
-parameters = [0.5, 0.5, -0.5]
-energy = 2/7
+parameters = [0.5, 1, -0.5]
+energy = 1/7
 numTrajectories = 10
 PSPoints = 1000
 
@@ -12,4 +12,7 @@ savePath = "d:\\results\\TwoFluid"
 
 pyplot(size = (1200,1000))
 fig = PoincareSection(energy, parameters, numTrajectories; maximumSectionPoints=PSPoints, sectionPlane=2, minimumBound=-sqrt(2.0), maximumBound=sqrt(2.0), tolerance=1e-8, sectionCoordinateX=3, sectionCoordinateY=1)
+fig = PoincareSection(energy, parameters, numTrajectories; maximumSectionPoints=PSPoints, sectionPlane=1, minimumBound=-sqrt(2.0), maximumBound=sqrt(2.0), tolerance=1e-8, sectionCoordinateX=4, sectionCoordinateY=2)
+fig = PoincareSection(energy, parameters, numTrajectories; maximumSectionPoints=PSPoints, sectionPlane=4, minimumBound=-sqrt(2.0), maximumBound=sqrt(2.0), tolerance=1e-8, sectionCoordinateX=3, sectionCoordinateY=1)
+fig = PoincareSection(energy, parameters, numTrajectories; maximumSectionPoints=PSPoints, sectionPlane=3, minimumBound=-sqrt(2.0), maximumBound=sqrt(2.0), tolerance=1e-8, sectionCoordinateX=4, sectionCoordinateY=2)
 #savefig(fig, savePath * "_PS_E=$(energy)_$vibronParameters.png")
