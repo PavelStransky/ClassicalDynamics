b2b5192fd2ca2fb76e03c761e67a9e460ac7ceda
Pavel Stránský <github@pavelstransky.cz>
2025-08-24 10:34:42 +0200

    Test of the special initical condition

--- BHTrajectory.jl
+++ BHTrajectory.jl
@@ -4,19 +4,26 @@ using Random
 using Statistics
 using Plots
 
-include("models/BoseHubbard.jl")
+include("models/BoseHubbardFull.jl")
 include("modules/ClassicalDynamics.jl")
 
 Random.seed!(1234)
 
-bhParameters = (2,-0.2,1)
-energy = 0.4
-initialCondition = InitialCondition(energy, bhParameters, 0.00001)
+bhParameters = (4,1,-10)
+energy = -5.85
+initialCondition = InitialCondition0(energy, bhParameters, 0.00001)
 
 println("Initial condition: ", initialCondition)
 println("Energy: ", Energy(initialCondition, bhParameters))
 
-Trajectory(initialCondition, bhParameters; verbose=true, tolerance=1E-10)
+# Trajectory(initialCondition, bhParameters; verbose=true, tolerance=1E-10)
 
 lyapunovs = TrajectoryLyapunov(initialCondition, bhParameters; 
 showFigures=true, sectionPlane=-1, maximumSectionPoints=-1, tolerance=1E-10, saveStep=1)
+
+readline()
+
+lyapunovs = TrajectoryLyapunov(initialCondition, bhParameters; 
+showFigures=true, sectionPlane=-1, maximumSectionPoints=-1, tolerance=1E-10, saveStep=1)
+
+# println("Lyapunov exponents: ", lyapunovs)
